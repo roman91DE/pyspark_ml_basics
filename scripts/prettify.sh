c3e191758eb623ce33cfebe60695d8739d7b5b0c
Roman Hoehn <rohoehn123@gmail.com>
2024-06-02 18:43:34 +0200

    this should fail as well

--- scripts/prettify.sh
+++ scripts/prettify.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
 
-# This file is symlinked to .git/hooks/pre-commit
+# This script is symlinked to .git/hooks/pre-commit
+
+echo "Running pre-commit hook"
 
 python_bin=$(which python3)
+echo "Using Python binary at $python_bin"
 
-for FILE in ./src/*.py; do
+# Get the list of Python files
+files=$(find ./src -name "*.py")
+
+# Check if any Python files are found
+if [ -z "$files" ]; then
+    echo "No Python files found to check."
+    exit 1
+fi
+
+for FILE in $files; do
     echo "Checking File $FILE"
 
     # Run isort and check for errors
