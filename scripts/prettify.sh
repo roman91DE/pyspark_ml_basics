ac986e5f15f388ae671f3a7d73ce406bf827e456
Roman Hoehn <rohoehn123@gmail.com>
2024-06-02 18:07:13 +0200

    this should fail

--- scripts/prettify.sh
+++ scripts/prettify.sh
@@ -6,7 +6,25 @@ python_bin=$(which python3)
 
 for FILE in ./src/*.py; do
     echo "Checking File $FILE"
+
+    # Run isort and check for errors
     "$python_bin" -m isort "$FILE"
+    if [ $? -ne 0 ]; then
+        echo "isort failed on $FILE"
+        exit 1
+    fi
+
+    # Run black and check for errors
     "$python_bin" -m black "$FILE"
+    if [ $? -ne 0 ]; then
+        echo "black failed on $FILE"
+        exit 1
+    fi
+
+    # Run mypy and check for errors
     "$python_bin" -m mypy "$FILE"
+    if [ $? -ne 0 ]; then
+        echo "mypy failed on $FILE"
+        exit 1
+    fi
 done
